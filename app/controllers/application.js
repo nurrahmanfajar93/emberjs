a99c8dddcb937b32aad3a7642f98cfded53ba8e5
nurrahmanfajar93 <nurrahmanfajar93@gmail.com>
2017-06-06 21:16:21 +0700

    refactoring

---
+++ app/controllers/application.js
@@ -0,0 +1,10 @@
+/**
+ * Created by fajar on 6/6/17.
+ */
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  init(){
+    this._super(...arguments);
+  },
+});
--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ module.exports = function(environment) {
     }
   };
 
-  if (environment === 'development') {
+  if (environment === 'dev') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
